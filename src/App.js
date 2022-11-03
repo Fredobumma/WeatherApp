0dbd9856587e6fa22cf7facbc4cc23be99760005
Manfred Obumma <fredobumma@gmail.com>
2022-11-03 14:56:57 +0100

    --feat: build app heading hard-codedly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <React.Fragment>
+      <main>
+        <nav className="navbar">
+          <img src={logo} className="app-logo" alt="logo" />
+          <span className="search-icon"></span>
+        </nav>
+        <header>
+          <h2>London</h2>
+          <p>Cloudy</p>
+        </header>
+        <section className="hero--section">
+          <div></div>
+        </section>
+      </main>
+    </React.Fragment>
   );
 }
 
