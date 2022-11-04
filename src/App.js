ce3795bed865d6b0e5021dd86f340988fcc30759
Manfred Obumma <fredobumma@gmail.com>
2022-11-04 05:44:15 +0100

    --feat: build app details hard-codedly

--- src/App.js
+++ src/App.js
@@ -8,14 +8,35 @@ function App() {
       <main>
         <nav className="navbar">
           <img src={logo} className="app-logo" alt="logo" />
-          <span className="search-icon"></span>
+          <span className="icon"></span>
         </nav>
         <header>
           <h2>London</h2>
           <p>Cloudy</p>
         </header>
-        <section className="hero--section">
-          <div></div>
+        <section className="illustrations"></section>
+        <section>
+          <div class="temperature--details">
+            <div className="temperature__average">51*</div>
+            <div>
+              <span className="temperature__minmax">47*</span>
+              <span className="temperature__minmax">63*</span>
+            </div>
+          </div>
+          <div className="info--details">
+            <div>
+              <span className="icon info--icon"></span>
+              <span className="info__text">106km/h</span>
+            </div>
+            <div>
+              <span className="icon info--icon"></span>
+              <span className="info__text">22%</span>
+            </div>
+            <div>
+              <span className="icon info--icon"></span>
+              <span className="info__text">11%</span>
+            </div>
+          </div>
         </section>
       </main>
     </React.Fragment>
