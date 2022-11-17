0d1b5d7a8feddbdbd550dadc1f98454d1822b566
Manfred Obumma <fredobumma@gmail.com>
2022-11-17 16:03:54 +0100

    --bug: fix env issues

--- src/services/weatherForecastService.js
+++ src/services/weatherForecastService.js
@@ -1,7 +1,6 @@
 import http from "./httpService";
 
-const apiEndpoint = (city) =>
-  `/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
+const apiEndpoint = (city) => `/weather?q=${city}&appid=${process.env.API_KEY}`;
 
 export function getWeatherForecast(city) {
   return http.get(apiEndpoint(city));
