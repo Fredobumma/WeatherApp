fe2c34df97002193eb203a7f43157f741310474c
Manfred Obumma <fredobumma@gmail.com>
2022-11-18 00:28:05 +0100

    --bug: fix persistent issues after deploying for prodcution

--- src/services/weatherForecastService.js
+++ src/services/weatherForecastService.js
@@ -1,6 +1,7 @@
 import http from "./httpService";
 
-const apiEndpoint = (city) => `/weather?q=${city}&appid=${process.env.API_KEY}`;
+const apiEndpoint = (city) =>
+  `/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
 
 export function getWeatherForecast(city) {
   return http.get(apiEndpoint(city));
